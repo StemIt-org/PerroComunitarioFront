7bff6d30279a0f49ff8669a7e24af49834fa24e9
Franco Rodriguez Viau <franco.rodriguez.viau@gmail.com>
2020-08-19 10:56:55 -0300

    Links de Navbar a la derecha

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -11,15 +11,11 @@ function FooterComponent() {
                     <p className="footer-titulo colores-rojo-text">PERRO COMUNITARIO</p>
                     <p className="footer-text">Somos una organizacion que e dedica en forma solidaria a mantener un control y seguimiento de los perros in hogar que transitan la calles de Nuevo Delta Tigre</p>
                 </Col>
-                <Col xs={12} lg={10}>
+                <Col xs={12} lg={{span:4, offset:6}} >
                     <p>Ou lord soy un footer</p>
 
                 </Col>
             </Row>
-            {/* <Row>
-                <Col xs={12} lg={2} className="footer-text">Somo una organizacion que e dedica en forma solidaria a mantener un control y seguimiento de los perros in hogar que transitan la calles de Nuevo Delta Tigre</Col>
-                <Col xs={12} lg={10}>lah rede sociale</Col>
-            </Row>             */}
         </>
     )
 }
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,39 +1,60 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import {NavLink, Link} from 'react-router-dom'
-
-
+import { NavLink, Link } from "react-router-dom";
 
 export default class NavbarComponent extends React.Component {
   render() {
     return (
-        <Navbar bg="light" expand="lg" >
-          <Link to="/" ><Navbar.Brand className="fuente"><img className="navbar-logo" src="https://i.gati.ga/logo-perro.png" alt="Logo perro comunitario"></img></Navbar.Brand></Link> 
-        {/* El link esta afuera para que no se ponga el colorsito azul en la navbar */}
-        {/* los colores del active y links estan en el index.css*/}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" >
-            <Nav className="mr-auto">
-                <NavLink className="links" activeClassName="active" exact to="/">
+        <Navbar bg="light" expand="lg">
+            <Link to="/">
+              <Navbar.Brand className="fuente">
+                <img
+                  className="navbar-logo"
+                  src="https://i.gati.ga/logo-perro.png"
+                  alt="Logo perro comunitario"
+                ></img>
+              </Navbar.Brand>
+            </Link>
+            {/* El link esta afuera para que no se ponga el colorsito azul en la navbar */}
+            {/* los colores del active y links estan en el index.css*/}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className ="ml-auto">
+                <NavLink
+                  className="links"
+                  activeClassName="active"
+                  exact
+                  to="/"
+                >
                   Inicio
                 </NavLink>
-                <NavLink className="links" activeClassName="active" to="/adopcion">
+                <NavLink
+                  className="links"
+                  activeClassName="active"
+                  to="/adopcion"
+                >
                   Perros en Adopción
                 </NavLink>
-                <NavLink className="links" activeClassName="active" to="/nosotros">
+                <NavLink
+                  className="links"
+                  activeClassName="active"
+                  to="/nosotros"
+                >
                   Nosotros
                 </NavLink>
                 <NavLink className="links" to="/donar">
                   Donar
                 </NavLink>
-                <NavLink className="links" activeClassName="active" to="/contacto">
+                <NavLink
+                  className="links"
+                  activeClassName="active"
+                  to="/contacto"
+                >
                   Contacto
                 </NavLink>
-                
-            </Nav>
-          </Navbar.Collapse>
+              </Nav>
+            </Navbar.Collapse>
         </Navbar>
-
     );
   }
 }
